afb9b7853d9d95f745675fea54b659bc4c993635
Eken Chan <maskedeken@yahoo.com>
2020-09-24 15:21:34 +0800

    Add trojan over websocket support

--- adapters/outbound/trojan.go
+++ adapters/outbound/trojan.go
@@ -5,11 +5,13 @@ import (
 	"encoding/json"
 	"fmt"
 	"net"
+	"net/http"
 	"strconv"
 
 	"github.com/Dreamacro/clash/component/dialer"
 	"github.com/Dreamacro/clash/component/mux"
 	"github.com/Dreamacro/clash/component/trojan"
+	"github.com/Dreamacro/clash/component/vmess"
 	C "github.com/Dreamacro/clash/constant"
 )
 
@@ -17,24 +19,70 @@ type Trojan struct {
 	*Base
 	instance *trojan.Trojan
 	mux      *mux.Client
+	option   *TrojanOption
 }
 
 type TrojanOption struct {
-	Name           string   `proxy:"name"`
-	Server         string   `proxy:"server"`
-	Port           int      `proxy:"port"`
-	Password       string   `proxy:"password"`
-	ALPN           []string `proxy:"alpn,omitempty"`
-	SNI            string   `proxy:"sni,omitempty"`
-	SkipCertVerify bool     `proxy:"skip-cert-verify,omitempty"`
-	UDP            bool     `proxy:"udp,omitempty"`
-	Mux            int      `proxy:"mux,omitempty"`
+	Name           string            `proxy:"name"`
+	Server         string            `proxy:"server"`
+	Port           int               `proxy:"port"`
+	Password       string            `proxy:"password"`
+	ALPN           []string          `proxy:"alpn,omitempty"`
+	SNI            string            `proxy:"sni,omitempty"`
+	SkipCertVerify bool              `proxy:"skip-cert-verify,omitempty"`
+	UDP            bool              `proxy:"udp,omitempty"`
+	Mux            int               `proxy:"mux,omitempty"`
+	Network        string            `proxy:"network,omitempty"`
+	WSPath         string            `proxy:"ws-path,omitempty"`
+	WSHeaders      map[string]string `proxy:"ws-headers,omitempty"`
+}
+
+func (t *Trojan) streamConn(c net.Conn) (net.Conn, error) {
+	var err error
+
+	switch t.option.Network {
+	case "ws":
+		host, port, _ := net.SplitHostPort(t.addr)
+		wsOpts := &vmess.WebsocketConfig{
+			Host: host,
+			Port: port,
+			Path: t.option.WSPath,
+		}
+
+		if len(t.option.WSHeaders) != 0 {
+			header := http.Header{}
+			for key, value := range t.option.WSHeaders {
+				header.Add(key, value)
+			}
+			wsOpts.Headers = header
+		}
+
+		wsOpts.TLS = true
+		wsOpts.SessionCache = getClientSessionCache()
+		wsOpts.SkipCertVerify = t.option.SkipCertVerify
+		if t.option.SNI != "" {
+			wsOpts.ServerName = t.option.SNI
+		} else {
+			wsOpts.ServerName = t.option.Server
+		}
+
+		c, err = vmess.StreamWebsocketConn(c, wsOpts)
+
+	default:
+		c, err = t.instance.StreamConn(c)
+	}
+
+	if err != nil {
+		return nil, fmt.Errorf("%s connect error: %w", t.addr, err)
+	}
+
+	return c, nil
 }
 
 func (t *Trojan) StreamConn(c net.Conn, metadata *C.Metadata) (net.Conn, error) {
-	c, err := t.instance.StreamConn(c)
+	c, err := t.streamConn(c)
 	if err != nil {
-		return nil, fmt.Errorf("%s connect error: %w", t.addr, err)
+		return nil, err
 	}
 
 	err = t.instance.WriteHeader(c, trojan.CommandTCP, serializesSocksAddr(metadata))
@@ -85,9 +133,9 @@ func (t *Trojan) DialUDP(metadata *C.Metadata) (C.PacketConn, error) {
 		return nil, fmt.Errorf("%s connect error: %w", t.addr, err)
 	}
 	tcpKeepAlive(c)
-	c, err = t.instance.StreamConn(c)
+	c, err = t.streamConn(c)
 	if err != nil {
-		return nil, fmt.Errorf("%s connect error: %w", t.addr, err)
+		return nil, err
 	}
 
 	err = t.instance.WriteHeader(c, trojan.CommandUDP, serializesSocksAddr(metadata))
@@ -128,6 +176,7 @@ func NewTrojan(option TrojanOption) (*Trojan, error) {
 			udp:  option.UDP,
 		},
 		instance: trojan.New(tOption),
+		option:   &option,
 	}
 
 	if option.Mux > 0 { // enable mux
@@ -137,9 +186,9 @@ func NewTrojan(option TrojanOption) (*Trojan, error) {
 				return nil, fmt.Errorf("%s connect error: %w", t.addr, err)
 			}
 			tcpKeepAlive(c)
-			c, err = t.instance.StreamConn(c)
+			c, err = t.streamConn(c)
 			if err != nil {
-				return nil, fmt.Errorf("%s connect error: %w", t.addr, err)
+				return nil, err
 			}
 
 			err = t.instance.WriteHeader(c, trojan.CommandMUX, serializesSocksAddr(metadata))
